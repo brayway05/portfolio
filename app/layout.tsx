6c13747daaf0414d061467e1d3dfeaab594b15a1
brayway05 <brayway05@gmail.com>
2025-04-15 01:11:34 -0600

    Added turbo pack

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,13 +9,6 @@ export const metadata: Metadata = {
   title: 'Brayden Christensen - Portfolio',
   description:
     'Portfolio website showcasing my work and experience as a Software Engineer, Data Scientist and AI Engineer.',
-  icons: {
-    icon: [
-      {
-        url: '/next.svg',
-      },
-    ],
-  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
--- next.config.js
+++ next.config.js
@@ -5,6 +5,12 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  turbopack: {
+    resolveAlias: {
+      underscore: 'lodash',
+    },
+    resolveExtensions: ['.mdx', '.tsx', '.ts', '.jsx', '.js', '.json'],
+  },
 };
 
 module.exports = nextConfig;
